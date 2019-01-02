5a1eb4bc920788172553d43d0264bc27e52ae977
manny agbayani <manny.i.agbayani@gmail.com>
2019-01-03 07:44:29 +1300

    updated comment

--- CrptoVox.sol
+++ CrptoVox.sol
@@ -54,18 +54,18 @@ contract CryptoVox is ERC20Interface {
     }
     
     /**
-    * @dev    - get the balance of an addresss
-    * @param  - tokenOwner is the address to look for
-    * @return - uint balance is the amount available of the @param
+    * @dev     get the balance of an addresss
+    * @param   tokenOwner is the address to look for
+    * @return  uint balance is the amount available of the @param
      */
     function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
     
     /**
-    * @dev    - transfer token between accounts
-    * @param  - to is the receiver, tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @dev     transfer token between accounts
+    * @param   to is the receiver, tokens is the number of tokens to transfer
+    * @return  bool success successful or not
      */    
     function transfer(address to, uint tokens) public returns (bool success) {
         require(to != address(0),"Invalid address");
@@ -78,18 +78,18 @@ contract CryptoVox is ERC20Interface {
     }
     
     /**
-    * @dev    - checking approved account
-    * @param  - tokenOwner is the owner and spender is the address that have permission to spend, tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @dev     checking approved account
+    * @param   tokenOwner is the owner and spender is the address that have permission to spend, tokens is the number of tokens to transfer
+    * @return  bool success successful or not
      */        
     function allowance(address tokenOwner, address spender) public view returns (uint remaining){
         return allowed[tokenOwner][spender];
     }
    
     /**
-    * @dev    - set spending token for an address
-    * @param  - spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @dev     set spending token for an address
+    * @param   spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
+    * @return  bool success successful or not
      */    
     function approve(address spender, uint tokens) public returns (bool success) {
         require(balances[msg.sender] >= tokens && tokens > 0,"Insufficient token");
@@ -100,11 +100,11 @@ contract CryptoVox is ERC20Interface {
     }
 
     /**
-    * @dev    - do transfer from different addresses
-    * @param  - from is the address where the token will come from, 
+    * @dev     do transfer from different addresses
+    * @param   from is the address where the token will come from, 
     *           to is the address of the receiver 
     *           tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @return  bool success successful or not
      */    
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         require(to != address(0),"Invalid 'to' addres");
@@ -118,9 +118,9 @@ contract CryptoVox is ERC20Interface {
     }
     
     /**
-    * @dev    - increase allowance
-    * @param  - spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @dev     increase allowance
+    * @param   spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
+    * @return  bool success successful or not
      */    
     function increaseAllowance(address spender, uint tokens) public returns (bool) {
         allowed[msg.sender][spender] = allowed[msg.sender][spender].add(tokens);
@@ -129,9 +129,9 @@ contract CryptoVox is ERC20Interface {
     }
 
     /**
-    * @dev    - decrease allowance
-    * @param  - spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
-    * @return - bool success successful or not
+    * @dev     decrease allowance
+    * @param   spender is the address that have permmission from the onwer, tokens is the number of tokens to transfer
+    * @return  bool success successful or not
      */    
     function decreaseAllowance(address spender, uint tokens) public returns (bool) {
         if(tokens > allowed[msg.sender][spender]){
